3f630c857eaf7941414ae2c97a06865c74192e5c
Leonardo Abreu <leonardoeap@gmail.com>
2020-10-29 17:19:08 -0400

    Add authorization and authentication

--- initial/src/main/java/com/example/securingweb/MvcConfig.java
+++ initial/src/main/java/com/example/securingweb/MvcConfig.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/public").setViewName("public");
 		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/admin_page").setViewName("admin_page");
+		registry.addViewController("/unauthorized").setViewName("unauthorized");
 	}
 
 }
---
+++ initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/public").permitAll()
+            .antMatchers("/admin_page").hasRole("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .exceptionHandling().accessDeniedPage("/unauthorized").and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("password")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
